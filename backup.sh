e57fa859c06cf4f0c408dbbbacb0c29ceb13965f
Andrey Borisov <andrey.borisov@wirenboard.com>
2025-07-30 11:19:37 +0300

    Edit last day of month remove logig

--- backup.sh
+++ backup.sh
@@ -65,8 +65,8 @@ NOW_UTC=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 
 # define yesterday and tomorrow date
 YESTERDAY_DATE=$(TZ=GMT+24 date +%Y-%m-%d)
-TOMORROW_DATE=$(TZ=GMT-24 date +%Y-%m-%d)
-NEXT_DAY_OF_MONTH=$(echo "$TOMORROW_DATE" | cut -d- -f3)
+TODAY_DATE=$(date +%Y-%m-%d)
+TODAY_NUMBER_DAY=$(echo "$TODAY_DATE" | cut -d- -f3)
 
 echo "Creating dump of ${POSTGRES_DATABASE} database from ${POSTGRES_HOST}..."
 pg_dump -Fc ${POSTGRES_HOST_OPTS} "${POSTGRES_DATABASE}" > db.dump
@@ -84,8 +84,22 @@ echo "DB backup uploaded successfully: s3://${S3_BUCKET}/${UPLOADED_FILE_KEY}"
 rm db.dump
 
 # Removing logic yesterday backup if tomorrow is not 01 day of month
-if [ "$NEXT_DAY_OF_MONTH" = "01" ]; then
-  echo "Yesterday (${YESTERDAY_DATE}) was the last day of the month. Keeping backup."
+if [ "$TODAY_NUMBER_DAY" = "01" ]; then
+  echo "Yesterday (${YESTERDAY_DATE}) was the last day of the month. Keeping only latest backup."
+  DELETE_FILE_PREFIX="${S3_PREFIX}/${POSTGRES_DATABASE}_${YESTERDAY_DATE}"
+  echo "Deleting backups with prefix '${DELETE_FILE_PREFIX}' except latest one..."
+  FILES_TO_DELETE=$(aws ${AWS_ARGS} s3api list-objects-v2 --bucket "${S3_BUCKET}" --prefix "${DELETE_FILE_PREFIX}" --query "Contents[].Key" --output text)
+  FILES_LIST=$(echo "$FILES_TO_DELETE" | tr '\t' '\n')
+  count=$(echo "$FILES_LIST" | wc -l)
+  FILES_TO_REMOVE=$(echo "$FILES_LIST" | head -n -1)
+  echo "Deleting backups except the latest:"
+  echo "$FILES_TO_REMOVE"
+  echo "$FILES_TO_REMOVE" | while IFS= read -r file_key; do
+    if [ -n "$file_key" ]; then
+      echo "Deleting s3://${S3_BUCKET}/$file_key"
+      aws ${AWS_ARGS} s3 rm "s3://${S3_BUCKET}/$file_key"
+    fi
+  done
 else
   DELETE_FILE_PREFIX="${S3_PREFIX}/${POSTGRES_DATABASE}_${YESTERDAY_DATE}"
   echo "Deleting backups with prefix '${DELETE_FILE_PREFIX}'..."
